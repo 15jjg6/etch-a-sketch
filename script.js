39696ae31777ea8079363ea080f3dc9993e06c3b
joegr <joe.grosso@cogeco.ca>
2020-04-25 11:07:33 -0400

    Add buttons + set up next stage of features

--- script.js
+++ script.js
@@ -1,32 +1,65 @@
+const sketchBoard = document.querySelector('#sketch-board');
+const resetBoard = document.querySelector('#removeDrawing');
+const blackMode = document.querySelector('#blackMode');
+const colorMode = document.querySelector('#colorMode');
+const changeNumberOfPixels = document.querySelector('#changeNumberOfPixels');
+let boardSize = 10;
+let gameModeBlack = true;
+createSketchDivs(boardSize);
+
+resetBoard.addEventListener('click', () => {
+	remove(); 
+	createSketchDivs(boardSize);
+});
+
+blackMode.addEventListener('click', () => {
+	if (gameModeBlack) {
+		return; 
+	};
+	remove(); 
+	gameModeBlack = true; 
+	createSketchDivs(boardSize);
+});
+
+colorMode.addEventListener('click', () => {
+	if (!gameModeBlack) {
+		return; 
+	};
+	remove(); 
+	gameModeBlack = false; 
+	createSketchDivs(boardSize);
+});
+
 function createSketchDivs(sideLength) {
 	const sketchBoard = document.querySelector('#sketch-board');
-	//console.log(sketchBoard.lastChild);
-	while (sketchBoard.lastElementChild) {
-		sketchBoard.removeChild(sketchBoard.lastElementChild);
-	}
-
 	for(let i = 0; i < sideLength; i++) {
 		let row = document.createElement('div');
 		row.setAttribute('class', 'row');
 		for (let j = 0; j < sideLength; j++) {
 			let pixel = document.createElement('div');
 			pixel.setAttribute('class', 'pixel');
+			pixel.addEventListener('mouseover', changeToBlack);
 			row.appendChild(pixel);
 		}
 		sketchBoard.appendChild(row);
 	}
 }
 
-function changeColor(e) {
-	e.srcElement.classList.add("color");
+function changeToBlack(e) {
+	e.srcElement.style.backgroundColor = "black";
 }
 
+function remove() {
+	const board = document.querySelector('#sketch-board');
+	const rows = document.querySelectorAll("div.row");
+	rows.forEach((row) => {
+		board.removeChild(row);
+	})
+}
 
-createSketchDivs(40);
+function changePixels() {
+	const sideLength = parseInt(prompt('Please enter a value between 1 and 200 for the number of pixels per row.'));
+	remove(); 
+	create
 
-const pixels = document.getElementsByClassName('pixel');
-console.log(pixels);
-Array.from(pixels).forEach(function (pixel) {
-	pixel.addEventListener('mouseover', changeColor);
-	pixel.mouseover = changeColor;
-});
+}
